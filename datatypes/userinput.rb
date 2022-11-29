571ec26c2e19ef7385774406be98c3ed7a0ca5b9
Victor Silva <silvavictorda42@gmail.com>
2022-11-29 18:53:34 +0100

    User Input - Dealing with the user data

---
+++ datatypes/userinput.rb
@@ -0,0 +1,11 @@
+#Getting values from the user
+print "Whats your name?"
+name = gets.chomp 
+#gets-> get the user input and adds an extra line
+#gets.chomp -> get the user input without a line
+print "Hello " + name + " How are you \nHow old are you?"
+age = gets.chomp 
+puts age + " its a good age"
+#print -> show text in same line
+#puts -> show text and break a line
+#LEARN
